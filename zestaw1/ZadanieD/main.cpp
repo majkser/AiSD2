0f9c50841ec942855d0af7b01bf8dcae871af490
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-04-26 13:19:52 +0200

    feat: added tests

--- zestaw1/ZadanieD/main.cpp
+++ zestaw1/ZadanieD/main.cpp
@@ -101,19 +101,19 @@ void measureTimeComplexity()
 
 int main()
 {
-    // dictionarySimple A(10);
+    dictionarySimple A(10);
 
-    // A.add("Ala");
-    // A.add("ma");
-    // A.add("kota");
-    // A.remove("ma");
+    A.add("Ala");
+    A.add("ma");
+    A.add("kota");
+    A.remove("ma");
 
-    // A.display();
+    A.display();
 
-    // std::cout << "Contains 'Ala': " << std::boolalpha << A.contains("Ala") << std::endl;
-    // std::cout << "Contains 'ma': " << std::boolalpha << A.contains("ma") << std::endl;
+    std::cout << "Contains 'Ala': " << std::boolalpha << A.contains("Ala") << std::endl;
+    std::cout << "Contains 'ma': " << std::boolalpha << A.contains("ma") << std::endl;
 
-    measureTimeComplexity();
+    // measureTimeComplexity();
 
     return 0;
 }
