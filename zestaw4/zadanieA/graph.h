65d1994e034e800ed8432ac9b8417dd8ec8c91e1
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-05-22 15:25:50 +0200

    feat: added addVertex method

--- zestaw4/zadanieA/graph.h
+++ zestaw4/zadanieA/graph.h
@@ -3,18 +3,20 @@
 
 #include <iostream>
 #include <string>
+#include <unordered_map>
 
 class graph
 {
 private:
     int **adjacencyMatrix;
     int numberOfVertices;
+    std::unordered_map<int, int> verticesValues;
     int numberOfEdges;
 
 public:
     graph(int vertices);
-    ~graph();
-    void adjacent(int vertex1, int vertex2);
+    ~graph() = default;
+    bool adjacent(int vertex1, int vertex2);
     void neighbors(int vertex);
     void addVertex(int vertex);
     void removeVertex(int vertex);
@@ -42,17 +44,78 @@ graph::graph(int vertices)
         {
             adjacencyMatrix[i][j] = 0;
         }
+        verticesValues.insert({i, i});
     }
 }
 
-graph::~graph() {}
+bool graph::adjacent(int vertex1, int vertex2)
+{
+    if (vertex1 >= 0 && vertex2 >= 0)
+    {
+        return adjacencyMatrix[verticesValues.at(vertex1)][verticesValues.at(vertex2)] != 0;
+    }
+    return false;
+}
+
+void graph::addVertex(int vertex)
+{
+    if (verticesValues.contains(vertex))
+    {
+        std::cout << "Vertex already exists." << std::endl;
+        return;
+    }
+
+    int newSize = numberOfVertices + 1;
+    int **newAdjacencyMatrix = new int *[newSize];
+
+    for (int i = 0; i < newSize; i++)
+    {
+        newAdjacencyMatrix[i] = new int[newSize];
+
+        for (int j = 0; j < newSize; j++)
+        {
+            if (i < numberOfVertices && j < numberOfVertices)
+            {
+                newAdjacencyMatrix[i][j] = adjacencyMatrix[i][j];
+            }
+            else
+            {
+                newAdjacencyMatrix[i][j] = 0;
+            }
+        }
+    }
+
+    for (int i = 0; i < numberOfVertices; i++)
+    {
+        delete[] adjacencyMatrix[i];
+    }
+    delete[] adjacencyMatrix;
+
+    adjacencyMatrix = newAdjacencyMatrix;
+    numberOfVertices = newSize;
+    verticesValues.insert({vertex, newSize - 1});
+}
+
+void graph::removeVertex(int vertex)
+{
+    if (!verticesValues.contains(vertex))
+    {
+        std::cout << "Vertex does not exist." << std::endl;
+        return;
+    }
+
+    int index = verticesValues.at(vertex);
+    int newSize = numberOfVertices - 1;
+    int **newAdjecencyMatrix = new int *[newSize];
+    // todo: finish this method and add the rest of the methods
+}
 
 void graph::addEdge(int vertex1, int vertex2)
 {
-    if (vertex1 >= 0 && vertex2 >= 0 && vertex1 < numberOfVertices && vertex2 < numberOfVertices)
+    if (vertex1 >= 0 && vertex2 >= 0)
     {
-        adjacencyMatrix[vertex1][vertex2] = 1;
-        adjacencyMatrix[vertex2][vertex1] = 1;
+        adjacencyMatrix[verticesValues.at(vertex1)][verticesValues.at(vertex2)] = 1;
+        adjacencyMatrix[verticesValues.at(vertex2)][verticesValues.at(vertex1)] = 1;
         numberOfEdges++;
     }
 }
@@ -61,17 +124,17 @@ void graph::removeEdge(int vertex1, int vertex2)
 {
     if (vertex1 >= 0 && vertex2 >= 0 && vertex1 < numberOfVertices && vertex2 < numberOfVertices)
     {
-        adjacencyMatrix[vertex1][vertex2] = 0;
-        adjacencyMatrix[vertex2][vertex1] = 0;
+        adjacencyMatrix[verticesValues.at(vertex1)][verticesValues.at(vertex2)] = 0;
+        adjacencyMatrix[verticesValues.at(vertex2)][verticesValues.at(vertex1)] = 0;
         numberOfEdges--;
     }
 }
 
 int graph::getEdgeValue(int vertex1, int vertex2)
 {
-    if (vertex1 >= 0 && vertex2 >= 0 && vertex1 < numberOfVertices && vertex2 < numberOfVertices)
+    if (vertex1 >= 0 && vertex2 >= 0)
     {
-        return adjacencyMatrix[vertex1][vertex2];
+        return adjacencyMatrix[verticesValues.at(vertex1)][verticesValues.at(vertex2)];
     }
     return -1; // Invalid edge
 }
@@ -80,8 +143,8 @@ void graph::setEdgeValue(int vertex1, int vertex2, int value)
 {
     if (vertex1 >= 0 && vertex2 >= 0 && vertex1 < numberOfVertices && vertex2 < numberOfVertices)
     {
-        adjacencyMatrix[vertex1][vertex2] = value;
-        adjacencyMatrix[vertex2][vertex1] = value;
+        adjacencyMatrix[verticesValues.at(vertex1)][verticesValues.at(vertex2)] = value;
+        adjacencyMatrix[verticesValues.at(vertex2)][verticesValues.at(vertex1)] = value;
     }
 }
 
