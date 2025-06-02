b7426128cbc042b313013b077bd2d0f080240b5a
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-06-03 00:58:55 +0200

    fix: bfs

--- zestaw6/ZadanieB/main.cpp
+++ zestaw6/ZadanieB/main.cpp
@@ -4,7 +4,10 @@
 #include <algorithm>
 #include <unordered_map>
 #include <vector>
-#include "../../zestaw5/ZadanieB/graph.h"
+// #include "../../zestaw5/ZadanieB/graph.h"
+#include <boost/graph/adjacency_list.hpp>
+#include <queue>
+#include <unordered_set>
 
 int countUTF8Characters(const std::string &str)
 {
@@ -42,11 +45,18 @@ void letterFileFormating()
     std::cout << "Cztero-literowe slowa zapisane do pliku 'slowaCzteroLiterowe.txt'." << std::endl;
 }
 
+typedef boost::property<boost::vertex_name_t, std::string> VertexProperty;
+
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS, VertexProperty> graph;
+typedef boost::graph_traits<graph>::vertex_descriptor Vertex;
+
+std::unordered_map<std::string, Vertex> wordToVertex;
+
 graph buildGraph()
 {
     std::ifstream file("slowaCzteroLiterowe.txt");
     std::unordered_map<std::string, std::vector<std::string>> buckets;
-    graph Graph(0);
+    graph Graph;
 
     for (std::string line; std::getline(file, line);)
     {
@@ -54,10 +64,13 @@ graph buildGraph()
         line.erase(std::remove(line.begin(), line.end(), '\r'), line.end());
         line.erase(std::remove(line.begin(), line.end(), ' '), line.end());
 
+        Vertex v = boost::add_vertex(Graph);
+        boost::put(boost::vertex_name, Graph, v, line);
+        wordToVertex[line] = v;
+
         for (size_t i = 0; i < line.length(); ++i)
         {
-            std::string key = line;
-            key.erase(i, 1); // Remove the character at position i
+            std::string key = line.substr(0, i) + '_' + line.substr(i + 1);
             buckets[key].push_back(line);
         }
     }
@@ -65,19 +78,27 @@ graph buildGraph()
 
     for (const auto &pair : buckets)
     {
-        const std::string &key = pair.first;
         const std::vector<std::string> &words = pair.second;
 
-        for (const auto &word : words)
-        {
-            Graph.addVertex(word);
-        }
+        // for (const auto &word : words)
+        // {
+        //     Vertex v = boost::add_vertex(Graph);
+        //     boost::put(boost::vertex_name, Graph, v, word);
+        //     wordToVertex[word] = v;
+        //     // std::cout << boost::get(boost::vertex_name, Graph, v) << std::endl;
+        // }
 
         for (size_t i = 0; i < words.size(); ++i)
         {
             for (size_t j = i + 1; j < words.size(); ++j)
             {
-                Graph.addEdge(words[i], words[j]);
+                Vertex v1 = wordToVertex[words[i]];
+                Vertex v2 = wordToVertex[words[j]];
+                boost::add_edge(v1, v2, Graph);
+
+                // std::cout << "doodano krawedz miedzy: "
+                //           << boost::get(boost::vertex_name, Graph, v1) << " i "
+                //           << boost::get(boost::vertex_name, Graph, v2) << std::endl;
             }
         }
     }
@@ -85,10 +106,97 @@ graph buildGraph()
     return Graph;
 }
 
+void wordLadder()
+{
+    graph Graph = buildGraph();
+    std::vector<Vertex> result;
+
+    std::string startWord, endWord;
+
+    do
+    {
+        std::cout << "Podaj poczatkowe slowo: ";
+        std::cin >> startWord;
+        std::cout << "Podaj koncowe slowo: ";
+        std::cin >> endWord;
+        if (countUTF8Characters(startWord) != 4 || countUTF8Characters(endWord) != 4)
+            std::cout << "Oba slowa musza miec dokladnie 4 litery." << std::endl;
+    } while (countUTF8Characters(startWord) != 4 || countUTF8Characters(endWord) != 4);
+
+    int startWordIndex = wordToVertex[startWord];
+    int endWordIndex = wordToVertex[endWord];
+    Vertex startVertex = boost::vertex(startWordIndex, Graph);
+    Vertex endVertex = boost::vertex(endWordIndex, Graph);
+
+    std::queue<Vertex> queue;
+    std::unordered_set visited = std::unordered_set<Vertex>();
+    std::unordered_map<Vertex, Vertex> parentMap;
+
+    queue.push(startVertex);
+    visited.insert(startVertex);
+
+    bool pathFound = false;
+
+    while (!queue.empty() && !pathFound)
+    {
+        Vertex currentVertex = queue.front();
+        queue.pop();
+
+        if (currentVertex == endVertex)
+        {
+            pathFound = true;
+            break;
+        }
+
+        auto [neighborsBegin, neighborsEnd] = boost::adjacent_vertices(currentVertex, Graph);
+        for (auto it = neighborsBegin; it != neighborsEnd; ++it)
+        {
+            Vertex neighbor = *it;
+            if (visited.find(neighbor) == visited.end())
+            {
+                visited.insert(neighbor);
+                queue.push(neighbor);
+                std::cout << "Odwiedzono: " << boost::get(boost::vertex_name, Graph, neighbor) << std::endl;
+                parentMap[neighbor] = currentVertex;
+            }
+        }
+    }
+
+    if (pathFound)
+    {
+        std::vector<Vertex> path;
+        Vertex current = endVertex;
+
+        while (current != startVertex)
+        {
+            path.push_back(current);
+            current = parentMap[current];
+        }
+        path.push_back(startVertex);
+
+        // Print the path in correct order
+        std::cout << "Word ladder: ";
+        for (auto it = path.rbegin(); it != path.rend(); ++it)
+        {
+            std::cout << boost::get(boost::vertex_name, Graph, *it);
+            if (it != path.rend() - 1)
+            {
+                std::cout << " -> ";
+            }
+        }
+        std::cout << std::endl;
+    }
+    else
+    {
+        std::cout << "Nie znaleziono sciezki miedzy " << startWord << " a " << endWord << std::endl;
+    }
+}
+
 int main()
 {
     // letterFileFormating();
-    buildGraph();
+    // buildGraph();
+    wordLadder();
 
     return 0;
 }
